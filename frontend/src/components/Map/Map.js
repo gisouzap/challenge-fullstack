1f8dcadad0d5899b541bac8058751e3f3d64e743
Gi <gisouza@pop-os.localdomain>
2020-05-20 22:54:05 -0300

    :sparkles: begin of map component

---
+++ frontend/src/components/Map/Map.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Map.css';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+const Map = () => {
+  return (
+    <div>
+      <Map className="leaflet-map" center={[-23.5489, -46.6388]} zoom={6}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={[-23.5489, -46.6388]}>
+          <Popup></Popup>
+        </Marker>
+      </Map>
+    </div>
+  );
+};
+
+export default Map;
